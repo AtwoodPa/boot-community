5e4db66cc0a258c3472b8dfe098be070515885ba
Coder_Pan <supanpan199919@163.com>
2023-09-03 08:12:33 +0800

    更改

---
+++ community/src/main/java/com/pp/community/filter/SensitiveFilter.java
@@ -0,0 +1,20 @@
+package com.pp.community.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author P_P
+ * @create 2023-09-03 8:10
+ */
+@Component
+public class SensitiveFilter {
+    private static final Logger logger = LoggerFactory.getLogger(SensitiveFilter.class);
+
+    private class TrieNode{
+
+    }
+
+}
